45ca6fb8135125cdaa59714fedfef4841358ce87
BuildTools <unconfigured@null.spigotmc.org>
2023-02-25 11:51:41 -0800

    completely forgot logout.js

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const logout = async () => {
+    const response = await fetch('/api/user/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Logout failed!');
+    }
+}
+
+document
+    .querySelector('#logout-link')
+    .addEventListener('click', logout);
