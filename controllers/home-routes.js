877c4cd118848b1983d8902cbf9cf3604d8c310c
BuildTools <unconfigured@null.spigotmc.org>
2023-02-25 04:37:14 -0800

    working on home-routes.js added get methods for all posts and single post

---
+++ controllers/home-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require ('../models/');
+
+// gets all posts for homepage
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('all-posts', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// gets single post by id
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        });
+
+        if (postData) { 
+            const post = postData.get({ plain: true });
+        } else {
+            res.status(400).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
